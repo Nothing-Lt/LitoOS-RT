cc9a8453df1a4978ab5221135cf1799ff8a77057
Li Naqin <780450734@qq.com>
2019-03-31 19:10:48 +0900

    Update x86_memory.c
    Format code, add space.

--- arch/x86/x86_memory.c
+++ arch/x86/x86_memory.c
@@ -12,27 +12,27 @@ void x86_set_page(unsigned memory_size)
     unsigned  temp;
     unsigned* pte_add;
     unsigned* pde_add;
-    unsigned  kernel_page_add=PAGE_TABLE_ADD;
-    unsigned  rl_ele_num_in_pte=memory_size/(SIZE_OF_PAGE);
-    unsigned  rl_ele_num_in_pde=rl_ele_num_in_pte/ELE_NUM_IN_PDE;
+    unsigned  kernel_page_add   = PAGE_TABLE_ADD;
+    unsigned  rl_ele_num_in_pte = memory_size / SIZE_OF_PAGE;
+    unsigned  rl_ele_num_in_pde = rl_ele_num_in_pte / ELE_NUM_IN_PDE;
     
     pde_add   = (unsigned*)(kernel_page_add);
     pte_add   = (unsigned*)(((unsigned)pde_add)+SIZE_OF_PDE);
 
     temp = (unsigned)pte_add;
-    for(i=0 ; i<rl_ele_num_in_pde ; i++)
+    for(i=0 ; i < rl_ele_num_in_pde ; i++)
     {
         pde_add[i] = temp|PAGE_EXIST_AUTH|PAGE_WRITE_AUTH;
         temp += SIZE_OF_PTE;
     }
 
     temp = 0;
-    for(i=0 ; i<rl_ele_num_in_pte ; i++)
+    for(i=0 ; i < rl_ele_num_in_pte ; i++)
     {
         pte_add[i] = temp|PAGE_EXIST_AUTH|PAGE_USER_AUTH;
         temp += SIZE_OF_PAGE;
     }
 
-    free_memory_add = (free_memory_add + SIZE_OF_PDE) + (ELE_NUM_IN_PDE*SIZE_OF_PTE);
+    free_memory_add = (free_memory_add + SIZE_OF_PDE) + (ELE_NUM_IN_PDE * SIZE_OF_PTE);
     store_page((unsigned*)kernel_page_add);
 }
